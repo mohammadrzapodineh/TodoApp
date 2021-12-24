e149e74919ce85ebfbfe023ce3797f4c9de56aa4
mohammad <mohammadrzapodineh@gmail.com>
2021-12-24 06:09:11 -0800

    Created Serializer model and Get List Todo Api

--- Todo/urls.py
+++ Todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, get_todo
 
 app_name = "Todo"
 
 urlpatterns = [
-    path("todo/", test_view, name="todo_home")
+    path("todo/", test_view, name="todo_home"),
+    path("todo/get", get_todo, name="todo_get"),
 ]
--- Todo/views.py
+++ Todo/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import TodoSerializer
+from .models import Todo
+from rest_framework.decorators import api_view
 
 
 def test_view(request):
     return render(request, 'Todo/todo.html')
+
+
+@api_view(['GET'])
+def get_todo(request):
+    query = Todo.objects.all()
+    serializer = TodoSerializer(query, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
